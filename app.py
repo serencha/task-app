41f10f0762f4c71cd3f7230d1e024bf978b8e66a
Serena Chang <schang2@babson.edu>
2022-10-27 02:10:28 -0400

    Update db.create_all()

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasklist.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
